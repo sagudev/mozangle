94e7dd0b4d9db16c0a7b00f51a6f0972277f09cb
sagudev <16504129+sagudev@users.noreply.github.com>
2023-10-17 11:57:59 +0200

    preprocessor & positional

--- build.rs
+++ build.rs
@@ -112,13 +112,14 @@ fn build_windows_dll(data: &build_data::Data, dll_name: &str, def_file: &str) {
     cmd.arg(&zlib_link_arg);
 
     if dll_name == "libGLESv2" {
+        cmd.arg(out_path.join("libGLESv2.lib"));
+        // transitive lib (that's the only case)
+        cmd.arg(out_path.join("preprocessor.lib"));
         for file in data.sources {
             if !file.contains("libANGLE") {
                 cmd.arg(fixup_path(file));
             }
         }
-        cmd.arg(out_path.join("libGLESv2.lib"));
-
     } else {
         for file in data.sources {
             cmd.arg(fixup_path(file));
