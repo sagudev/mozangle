9a12787d1970773a814793de273e5c32bd6fa204
sagudev <you@example.com>
2023-10-18 15:16:36 +0200

    no whole

--- build.rs
+++ build.rs
@@ -244,7 +244,7 @@ fn build_lib(libs: &mut HashSet<Libs>, target: &String, lib: Libs) {
     // Enable multiprocessing for faster builds.
     build.flag_if_supported("/MP");
 
-    build.link_lib_modifier("+whole-archive");
+    build.link_lib_modifier("-whole-archive");
 
     build.compile(data.lib);
 
@@ -301,7 +301,7 @@ fn build_translator(libs: &mut HashSet<Libs>, target: &String) {
             .flag_if_supported("-arch:SSE2"); // MSVC
     }
 
-    build.link_lib_modifier("+whole-archive");
+    build.link_lib_modifier("-whole-archive");
 
     build.compile("glslang_glue");
 
